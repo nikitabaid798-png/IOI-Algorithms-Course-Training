2630bce3ae21adca3721b9065348e211be813685
nikitabaid798-png <nikitabaid798@gmail.com>
2026-08-22 01:48:19 +0530

    Add files via upload

---
+++ M1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, Coder!")
+
+# Printing my name
+print("My name is Nikita")
+
+# Printing a fun fact about myself
+# print("I enjoy Dancing!")
+print("I enjoy Dancing!")
+
+# Performing and printing a simple calculation
+print("The result of 7 + 8 is:", 7 + 8)
