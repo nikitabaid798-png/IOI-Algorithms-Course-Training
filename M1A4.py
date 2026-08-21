afec3172f8b0ab84701c2af1f3dd837101437959
nikitabaid798-png <nikitabaid798@gmail.com>
2026-08-22 01:59:04 +0530

    Add files via upload

---
+++ M1A4.py
@@ -0,0 +1,13 @@
+#Print a Sentence 
+print("Welcome to the world of Python Programming!")
+
+#Print a Number
+print(7)
+
+#Next Line after print
+print("Hello Coder \n")
+
+#Print all the keywords present in Python
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
