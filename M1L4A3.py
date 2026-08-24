0b5afe699902b49d142e38ba41e9a4ffdbab25e3
nikitabaid798-png <nikitabaid798@gmail.com>
2026-08-24 16:47:22 +0530

    Create M1L4A3.py

---
+++ M1L4A3.py
@@ -0,0 +1,13 @@
+# Initialize the first natural number
+number = 1
+
+# Initialize the sum
+total = 0
+
+# Calculate the sum of the first 20 natural numbers
+while number <= 20:
+    total = total + number
+    number = number + 1
+
+# Display the result
+print("The sum of the first 20 natural numbers is:", total)
