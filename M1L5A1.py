107b947db2d0cd7ec95034b0e709e4897fd58ae9
nikitabaid798-png <nikitabaid798@gmail.com>
2026-08-24 16:53:12 +0530

    Create M1L5A1.py

---
+++ M1L5A1.py
@@ -0,0 +1,11 @@
+# Function to introduce yourself
+def introduce(name):
+    print("Hello!")
+    print("Good morning!")
+    print("How are you,", name + "?")
+
+# Ask the user to enter their name
+user_name = input("Enter your name: ")
+
+# Pass the name to the function
+introduce(user_name)
