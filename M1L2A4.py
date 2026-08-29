16e8915316745df0e191895163e0502b3a28c428
nikitabaid798-png <nikitabaid798@gmail.com>
2026-08-30 01:22:37 +0530

    Create M1L2A4.py

---
+++ M1L2A4.py
@@ -0,0 +1,13 @@
+# ---- Set Intersection ----
+
+# Create two sets
+class_a = {"Math", "Science", "English", "Computer"}
+class_b = {"Science", "Computer", "History", "Art"}
+
+print("Class A subjects:", class_a)
+print("Class B subjects:", class_b)
+
+# Intersection of two sets
+common_subjects = class_a.intersection(class_b)
+
+print("\nCommon subjects:", common_subjects)
